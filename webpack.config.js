3eb36e1f661301fdcd2081a9a17b873b38227480
maik10000 <91635577+maik10000@users.noreply.github.com>
2022-06-16 15:26:45 -0500

    dependecia y configuracioness

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+/**@type{import('webpack').Configuration} */
+module.exports = {
+    entry:"./src/index.js",
+    output:{
+        path: path.resolve(__dirname,"dist"),
+        filename:"[name].[contenthash].js",
+        publicPath:""
+    },
+    mode:'production',
+}
