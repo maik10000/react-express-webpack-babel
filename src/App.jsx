79c04db9ae283a25a08b8ae34b529518862fe3b0
maik10000 <91635577+maik10000@users.noreply.github.com>
2022-06-17 08:27:30 -0500

    j

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 export function App(){
     return(
-        <h1>Holaaa soy react</h1>
+        <div className="app">
+           <form method="POST">
+            <h1>Nombre</h1>
+                <input type="text" name="nombre" />
+                <h1>apellido</h1>
+                <input type="text" name="apellido"/>
+                <h1>numero</h1>
+                <input type="text" name="numero" />
+                <button onSubmit={()=>{
+                }}>enviar</button>
+           </form>
+        </div>  
     )
 }
